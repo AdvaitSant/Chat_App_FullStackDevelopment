93be5702ef2459636dccb25e747a96ca52ae4722
AdvaitSant <advait1201@gmail.com>
2025-05-07 22:14:25 +0530

    Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define the schema for a message
 const messageSchema = new mongoose.Schema({
   sender: { type: String, required: true },
   receiver: { type: String, required: true },
@@ -8,7 +7,6 @@ const messageSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
 });
 
-// Create the message model based on the schema
 const Message = mongoose.model('Message', messageSchema);
 
 module.exports = Message;
